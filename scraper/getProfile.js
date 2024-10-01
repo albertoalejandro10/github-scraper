253243a34174fda4d0c3b9fe9f95ef1bbf0b2252
AlbertoAlejandro <alberto196g@gmail.com>
2024-10-01 00:48:46 -0400

    improved api data

--- scraper/getProfile.js
+++ scraper/getProfile.js
@@ -8,14 +8,14 @@ export const getProfile = async (page) => {
       name: await getTextContent(page, '.vcard-fullname') || 'Name not available',
       username: await getTextContent(page, '.vcard-username') || 'Username not available',
       bio: await getTextContent(page, '.user-profile-bio') || 'Bio not available',
-      avatarUrl: await getAvatarUrl(page),
+      avatar: await getAvatarUrl(page),
 
       followers: await getTextContent(page, '.js-profile-editable-area a:nth-child(1)', el => el.textContent.trim().replace(/[^0-9]/g, '')) || '0',
       following: await getTextContent(page, '.js-profile-editable-area a:nth-child(2)', el => el.textContent.trim().replace(/[^0-9]/g, '')) || '0',
 
       location: await getTextContent(page, '.vcard-details > li.vcard-detail[itemprop="homeLocation"] > span') || 'Location not available',
       website: await getTextContent(page, '.vcard-details > li.vcard-detail[itemprop="url"] > a') || 'Website not available',
-      socialNetworks: await getArrayTextContent(page, '.vcard-details > li.vcard-detail[itemprop="social"] > a', elements => elements.map(el => el.href)) || ['No social networks'],
+      networks: await getArrayTextContent(page, '.vcard-details > li.vcard-detail[itemprop="social"] > a', elements => elements.map(el => el.href)) || ['No social networks'],
       badges: await getBadges(page),
     }
   } catch (error) {
--- utils/comparatorArray.js
+++ utils/comparatorArray.js
@@ -1,9 +1,9 @@
 export const comparatorArray = (followers, following) => {
-  const whoDontFollowYouBack = following.filter(following => followers.every(follower => follower.username !== following.username))
-  const whoYouDontFollowBack = followers.filter(follower => following.every(following => following.username !== follower.username))
+  const dontFollowYou = following.filter(following => followers.every(follower => follower.username !== following.username))
+  const youDontFollow = followers.filter(follower => following.every(following => following.username !== follower.username))
 
   return {
-    whoDontFollowYouBack,
-    whoYouDontFollowBack
+    dontFollowYou,
+    youDontFollow
   }
 }
